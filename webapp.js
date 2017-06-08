540bbd681904944cbc5881dd136c42e777e08af6
Bob <bob.tran@contemi.com.vn>
2017-06-08 18:35:11 +0700

    Step 5.1

--- webapp.js
+++ webapp.js
@@ -4,6 +4,15 @@ var app = express();
 
 app.use(express.static('static'));
 
+var bugData = [
+  {id: 1, priority: 'P1', status:'Open', owner:'Jessica Bánh Bèo', title:'Jessica Bánh Bèo'},
+  {id: 2, priority: 'P2', status:'New', owner:'Eddie Tí Tèo', title:'Thiếu canh lề CSS cho văn bản trong table'},
+];
+
+app.get('/api/bugs', function(req, res) {
+  res.status(200).send(JSON.stringify(bugData));
+});
+
 var server = app.listen(3000, function() {
 	var port = server.address().port;
 	console.log("Started server at port", port);

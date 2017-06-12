d0b7ab10dab27e89a039a92a401ce945a452d928
Bob <bob.tran@contemi.com.vn>
2017-06-12 17:42:25 +0700

    Step 8.3

--- src/BugFilter.js
+++ src/BugFilter.js
@@ -5,12 +5,42 @@ var BugFilter = React.createClass({
   render: function() {
     console.log("Rendering BugFilter");
     return (
-      <button onClick={this.submit}>Lọc Kết Quả</button>
+      <div>
+        <h3>Bộ Lọc</h3>
+        Trạng Thái (Status):
+        <select value={this.state.status} onChange={this.onChangeStatus}>
+          <option value="">---Chọn---</option>
+          <option value="New">New</option>
+          <option value="Open">Open</option>
+          <option value="Closed">Closed</option>
+        </select>
+        <br/>
+        Ưu Tiên (Priority):
+        <select value={this.state.priority} onChange={this.onChangePriority}>
+          <option value="">---Chọn---</option>
+          <option value="P1">P1</option>
+          <option value="P2">P2</option>
+          <option value="P3">P3</option>
+        </select>
+        <br/>
+        <button onClick={this.submit}>Áp Dụng</button>
+      </div>
     )
   },
 
+  getInitialState: function() {
+    return {status: "", priority: ""};
+  },
+
+  onChangeStatus: function(e) {
+    this.setState({status: e.target.value});
+  },
+  onChangePriority: function(e) {
+    this.setState({priority: e.target.value});
+  },
+
   submit: function(e) {
-    this.props.submitHandler({priority: "P1"});
+    this.props.submitHandler({priority: this.state.priority, status: this.state.status});
   }
 });
 

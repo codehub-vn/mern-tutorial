d92b2e9146b539d7a3ede8dea8d21ff513270e7d
Bob <bob.tran@contemi.com.vn>
2017-06-13 15:14:20 +0700

    Step 11.3

--- src/BugAdd.js
+++ src/BugAdd.js
@@ -1,17 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Panel = require('react-bootstrap/lib/Panel');
+var Input = require('react-bootstrap/lib/Input');
+var ButtonInput = require('react-bootstrap/lib/ButtonInput');
+
 var BugAdd = React.createClass({
   render: function() {
     //console.log("Rendering BugAdd");
     return (
-      <div>
+      <Panel header="Thêm Bug">
         <form name="bugAdd">
-          <input type="text" name="owner" placeholder="Owner" />
-          <input type="text" name="title" placeholder="Title" />
-          <button onClick={this.handleSubmit}>Thêm Bug</button>
+          <Input type="text" name="title" label="Title" />
+          <Input type="text" name="owner" label="Owner" />
+          <ButtonInput value="Thêm" bsStyle="primary" onClick={this.handleSubmit} />
         </form>
-      </div>
+      </Panel>
     )
   },
 
